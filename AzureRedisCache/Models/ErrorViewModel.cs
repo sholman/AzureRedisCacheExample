bc373ed1efb9279e10f0960e4cf3c011bf30d5ca
sholman <sholman@expeed.com.au>
2019-03-11 16:49:52 +1030

    Add project files.

---
+++ AzureRedisCache/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureRedisCache.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
